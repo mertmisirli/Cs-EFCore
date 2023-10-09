2138e3574f6b0f9b7ce7d81860945d11113818d3
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-10-09 22:28:46 +0300

    Add files via upload

---
+++ EFCore/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EFCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> persons { get; set; }
+        public DbSet<Address> address { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Veritabanı bağlantı dizesini burada tanımlayın.
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=MyDatabase;Trusted_Connection=True;");
+
+        }
+    }
+
+}
---
+++ EFCore/Program.cs
@@ -0,0 +1,53 @@
+﻿using EFCore;
+using EFCore.Entities;
+
+
+using (var context = new AppDbContext())
+{
+    var entityToDelete = context.persons.FirstOrDefault(e => e.Id == 2);
+
+    if (entityToDelete != null)
+    {
+        context.persons.Remove(entityToDelete);
+        context.SaveChanges();
+    }
+}
+
+
+using (var context = new AppDbContext())
+{
+    Random random = new Random();
+
+    var entitiesToAdd = new List<Person>
+    {
+        new Person
+        {
+            Name = "Person1 name",
+            Surname = "Person1 Surname",
+            Id = random.Next(1, 101) 
+        },
+        new Person
+        {
+            Name = "Person2 name",
+            Surname = "Person2 Surname",
+            Id = random.Next(1, 101) 
+        },
+    };
+
+    context.persons.AddRange(entitiesToAdd);
+    context.SaveChanges();
+}
+
+
+using (var context = new AppDbContext())
+{
+    var entityToUpdate = context.persons.FirstOrDefault(e => e.Id == 3);
+
+    if (entityToUpdate != null)
+    {
+        entityToUpdate.Name = "new Name";
+        entityToUpdate.Surname = "new Surname";
+        context.SaveChanges();
+    }
+}
+
